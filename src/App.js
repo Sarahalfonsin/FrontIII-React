324e40ee1bfa60fa794a1ff61b5330f818d38174
Sarahalfonsin <sarahalfonsin@hotmail.com>
2022-04-13 18:59:38 -0300

    heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  //HOOk
+  //const [variable,metodo] = useState(valorInicial);
+
+  const [opcion, setOpcion] = useState("hoteles");
+  const [data, setData] = useState([]);
+  //efectos secundarios , tienen dos parametros
+  //useEffect(callback,[variable])
+  let url = "https://pruebagcd.herokuapp.com/";
+  useEffect(() => {
+    const api = async () => {
+      const response = await fetch(url + opcion);
+      const json = await response.json();
+      setData(json);
+    };
+    api();
+    //solo se lleva a cabo cuando se modifica opcion va en el []
+  }, [opcion]);
+  const cambiarOpcion = () => {
+    if (opcion === "hoteles") {
+      setOpcion("paquetes");
+    } else if (opcion === "paquetes") {
+      setOpcion("ofertas");
+    } else setOpcion("hoteles");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <button className="btn btn-danger my-5" onClick={cambiarOpcion}>
+            Cambiar Opcion
+          </button>
+        </div>
+        <div className="col-6">
+          <ul className='my-5' type="none">
+            {data.map((opcion, index) => {
+              return <li key={index + opcion.nombre}>{opcion.nombre}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
